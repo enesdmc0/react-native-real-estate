f39001c665d543ec3bbf4aeb339ab889aa0a63a7
Enes Demirci <20194604004@ogr.akdeniz.edu.tr>
2025-01-05 14:46:58 +0300

    2: Setup Routes

---
+++ app/(root)/properties/[id].tsx
@@ -0,0 +1,13 @@
+import {Text, View} from 'react-native'
+import React from 'react'
+import {useLocalSearchParams} from "expo-router";
+
+const Property = () => {
+    const {id} = useLocalSearchParams()
+    return (
+        <View>
+            <Text>Property: {id}</Text>
+        </View>
+    )
+}
+export default Property
