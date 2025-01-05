d09a6aa19973ec8cff6fe2100a41cb53cdd6c5a1
Enes Demirci <20194604004@ogr.akdeniz.edu.tr>
2025-01-05 16:02:58 +0300

    4: Setup Fonts and Assets

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,11 +1,12 @@
-import {  View } from "react-native";
+import {Text, View} from "react-native";
 import {Link} from "expo-router";
 
 export default function Index() {
   return (
     <View
-      className="bg-red-400 border border-red-400"
+      className="bg-red-400 border border-red-400 "
     >
+        <Text className="font-rubik text-danger">Enes Demirci</Text>
         <Link href="/sign-in">Sign In</Link>
         <Link href="/explore">explore</Link>
         <Link href="/profile">profile</Link>
